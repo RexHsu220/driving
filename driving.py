5499aed25dbd05546d5a4013c99f2c387b259f79
Rex Hsu <weiyao220@gmail.com>
2019-08-17 13:19:45 +0800

    Adding USA into an if option'

--- driving.py
+++ driving.py
@@ -5,4 +5,9 @@ if country == 'Taiwan':
 	if age >= 18: 
 		print('You are allowed to drive if you have a license.')
 	else:
-		print('You are not allowed to drive')	
+		print('You are not allowed to drive')
+elif country == 'USA':
+	if age >= 16:
+		print('You are allowed to drive if you have a license')
+	else:
+		print('You are not allowed to drive')
